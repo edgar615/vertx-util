1b46d8338662007ba32541c8fbc2891e1552d928
edgar615 <edgar615@gmail.com>
2017-03-30 11:45:24 +0800

    增加Initializable接口

---
+++ src/main/java/com/edgar/util/vertx/spi/Initializable.java
@@ -0,0 +1,23 @@
+package com.edgar.util.vertx.spi;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * 启动接口.
+ * 凡是继承该接口的类再应用启动之后都应该执行initialize方法，执行初始化工作.
+ *
+ * @author Edgar  Date 2017/3/30
+ */
+public interface Initializable {
+
+  /**
+   * 启动方法.
+   *
+   * @param vertx    Vertx
+   * @param config   配置
+   * @param complete 回调
+   */
+  void initialize(Vertx vertx, JsonObject config, Future<Void> complete);
+}
